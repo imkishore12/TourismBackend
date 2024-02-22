f153554c3a99c0150a143d53e5a5f02d385e2740
imkishore12 <121910305010@gitam.in>
2024-02-22 20:13:40 +0530

    frontend

--- Data/indiantourism.js
+++ Data/indiantourism.js
@@ -15,7 +15,7 @@ const indiantourism=[
         "id":Count++,
             "category":"indiatousrism",
             "Image":"https://www.theindiatourism.com/images/Arunachal-Pradesh-Tourism.jpg",
-            "State":"Andhra Pradesh Tourism",
+            "State":"Arunachal Pradesh Tourism",
             "Description":"Arunachal Pradesh located in north eastern India has fifteen districts and is called 'Land of the Rising Sun'. The literary meaning of the 'Land of the Rising Sun' is 'the land of the dawn lit mountains.' The state has its capital in Itanagar, which is also its largest city. The state, known for its natural beauty is backed by a strong agricultural economy- pulses, sugarcane, rice, wheat, maize being its main crops. It has a considerable amount of forest cover which ensures that the forest products also contribute to its economy. The tourism sector of the state, however, has not been fully utilized.",
             "History":" The history of pre-modern Arunachal Pradesh is unclear. According to the Arunachal Pradesh government, the Hindu texts Mahabharata mention the region as the Prabhu Mountains of the Puranas, and where sage Parashurama washed away sins, the sage Vyasa meditated, King Bhishmaka founded his kingdom, and Lord Krishna married his consort Rukmini. On the other hand, the Kalika Purana written in the 10th century mentions the region to be ruled by the descendants of a king named Bhirmuko of the Chutika (today's Chutia) clan.Recorded history only became available in the Chutiya chronicles. The Monpa and Sherdukpen do keep historical records of the existence of local chiefdoms in the northwest as well. Northwestern parts of this area came under the control of the Monpa kingdom of Monyul, which flourished between 500 B.C. and 600 A.D. The remaining parts of the state, especially those bordering Myanmar, were under the control of the Chutiya Kings. However, most Arunachali tribes remained in practice largely autonomous up until Indian independence and the formalisation of indigenous administration in 1947.Recent excavations of ruins of Hindu temples, such as the 14th century Malinithan at the foot of the Siang hills in West Siang, indicate they were built during the Chutiya reign. Another notable heritage site, Bhismaknagar (built in 8th century), has led to suggestions that the Chutiya people had an advanced culture and administration in prehistoric times. The third heritage site, the 400-year-old Tawang Monastery in the extreme north-west of the state, provides some historical evidence of the Buddhist tribal people. The sixth Dalai Lama Tsangyang Gyatso was born in Tawang.",
             "fairsfestivals":" Fairs and festivals in Arunachal Pradesh are a fine way to understand the state's rich culture and traditional practices. An extension of the lives of the people of the state, the Arunachal Pradesh fairs and festivals are characterized by a lot of color. Right from festivals with religious undertones to merry affairs, you can find them all as you check the fairs and festivals calendar of Arunachal Pradesh, Northeast India. Many of the festivals celebrated in Arunachal Pradesh are closely linked to agriculture, the main occupation of the state. Celebrated on a grand scale, these festivals are characterized by prayers that are later followed by merry making. Among the most popular festivals of Arunachal Pradesh are Ziro Festival of Music , Losar Festival, Tamladu Festival, Sangken Festival.",
@@ -26,10 +26,10 @@ const indiantourism=[
             "category":"indiatousrism",
             "Image":"https://www.theindiatourism.com/images/Goa-Tourism.jpg",
             "State":"Goa Tourism.",
-            "Description":"",
-            "History":"",
-            "fairfestivals":""
-
+            "Description":"The state of Goa, in India, is famous for its beaches and places of worship, and tourism is its primary industry. Tourism is generally focused on the coastal areas of Goa, with decreased tourist activity inland. Foreign tourists, mostly from Europe, arrive in Goa in winter whilst the summer and monsoon seasons see a large number of Indian tourists. Goa handled 2.29% of all foreign tourist arrivals in the country in 2011. This relatively small state is situated on the western coast of India, between the borders of Maharashtra and Karnataka and is better known to the world as a former Portuguese enclave on Indian soil. Tourism is said to be the backbone of Goa's economy            ",
+            "History":"The history of Goa dates back to prehistoric times, though the present-day state of Goa was only established as recently as 1987. In spite of being India's smallest state by area, Goa's history is both long and diverse. It shares a lot of similarities with Indian history, especially with regard to colonial influences and a multi-cultural aesthetic.Situated on the Konkan Coast in Western India, Goa was[when?] one of the major trade centres in India. It attracted influential dynasties, seafarers, merchants, traders, monks and missionaries since its earliest known history. Throughout its history, Goa has undergone continual transformation, leaving an indelible impression on various aspects of its cultural and socio-economic development.",
+            "fairfestivals":"Fairs and Festivals in Goa are really a refreshing experience for the town dwellers as well as the visitors to the enchanting beach town. Various festivals and events in Goa are celebrated with all the pomp and show. The long list of the most-popular Fairs and Festivals includes Monsoon Festival Goa, Christmas & New Year Celebrations in Goa and the Festival of Three Kings Feast. Goa Christmas Celebrations and New Year Celebrations are world famous and people from across the world come and enjoy the glamour of these memorable moments. The most-awaited Goa Carnival Festival 2012 was celebrated last February.",
+            "Reach":"Goa’s main railway stations are in Mudgaon, and Thivim. Other railway stations are Vasco Da Gama, Karmali, Sanverdam Church, Cancona, Pernem and Karmali. Most of these railway stations are well connected with the major cities and towns of the country. You can easily get taxis and traveler bus from these stations.",
 
         }
         ,
@@ -40,7 +40,8 @@ const indiantourism=[
             "State":"Himachal Pradesh Tourism",
             "Description":"",
             "History":"",
-            "fairfestivals":""
+            "fairfestivals":"",
+            "Reach":""
 
 
         }
@@ -50,9 +51,10 @@ const indiantourism=[
             "category":"indiatousrism",
             "Image":"https://www.theindiatourism.com/images/Kerala-Tourism.jpg",
             "State":"Kerala-Tourism",
-            "Description":"",
-            "History":"",
-            "fairfestivals":""
+            "Description":"Kerala, a state situated on the tropical Malabar Coast of southwestern India, is one of the most popular tourist destinations in the country. Named as one of the ten paradises of the world by National Geographic Traveler, Kerala is famous especially for its ecotourism initiatives and beautiful backwaters. Its unique culture and traditions, coupled with its varied demography, have made Kerala one of the most popular tourist destinations in the world. Growing at a rate of 13.31%, the tourism industry is a major contributor to the state's economy.Until the early 1980s, Kerala was a relatively unknown destination, with most tourism circuits concentrated around the north of the country. Aggressive marketing campaigns launched by the Kerala Tourism Development Corporation—the government agency that oversees tourism prospects of the state—laid the foundation for the growth of the tourism industry. In the decades that followed, Kerala Tourism was able to transform itself into one of the niche holiday destinations in India. The tag line Kerala – God's Own Country was adopted in its tourism promotions and became a global superbrand. Kerala is regarded as one of the destinations with the highest brand recall. In 2010, Kerala attracted 660,000 foreign tourist arrivals.",
+            "History":"The history of Kerala, India, dates back many millennia. Stone Age carvings in the Edakkal Caves feature pictorial writings believed to date to at least the Neolithic era around 5,000 BC, indicating the presence of a prehistoric civilisation or settlement in this region. From as early as 3000 BC, Chera nadu, currently known as Kerala had established itself as a major spice trade centre. Keralam, the then Chera nadu had direct contact across the Arabian Sea with all the major Mediterranean and Red Sea ports as well those of the Far East. The spice trade between Kerala and much of the world was one of the main drivers of the world economy. For much of history, ports in Kerala were the busiest (Muziris) among all trade and travel routes in the history of the world.",
+            "fairfestivals":"Kerala with its beautiful landscapes has more than enough reason to celebrate. And the varied culture of the state has given expression to a colorful mosaic of festivals and fairs. The spirit of celebration is very much a part of the ethos of the state of Kerala and for visitors to this state,its fairs and festivals are a must see event to be included in their travel",
+            "Reach":"Kerala has more than 200 railway stations, connecting various major cities as well as small towns of the state. Direct trains to various destinations in Kerala can be availed from major Indian cities such as Delhi, Mumbai, Chennai, Bangalore, and Kolkata, among others. Superfast and express trains such as Rajdhani and Duronto connect the larger junctions, whereas mail and passenger trains are also easily available. Kerala's railway network runs from Kasargod, a northwestern point of the state, to the southwestern Thiruvananthapuram."
 
 
         }
@@ -62,9 +64,75 @@ const indiantourism=[
             "category":"indiatousrism",
             "Image":"https://www.theindiatourism.com/images/Tamil-Nadu-Tourism.jpg",
             "State":"Tamil-Nadu-Tourism",
-            "Description":"",
-            "History":"",
-            "fairfestivals":""
+            "Description":"Tamil Nadu is a state in the south-eastern part of the Indian Peninsula. Tamilnadu is previously a part of the United Madras Province, which was later partitioned based on languages. Tamilnadu has more than 4,000 years of continuous cultural history. Tamil Nadu has some of the most remarkable temple architecture in the country, and a living tradition of music, dance, folk arts and fine arts. Tamil Nadu is well renowned for its temple towns and heritage sites, hill stations, waterfalls, national parks, local cuisine and the natural environment and wildlife. The state has the largest tourism industry in India with an annual growth rate of 16%. In 2015, the number of domestic arrivals was at 333.5 million making the state the most popular tourist destination in the country, and foreign arrivals numbered 4.68 million, the highest in the country, making it the most popular state for tourism in the country.",
+            "History":"Tamil Nadu has a very ancient history that dates back to some 6000 years and the origin of its people is closely tied to the debates of the Aryan invasion theory. Those who uphold this theory favour the view that the Tamils belong to the Dravidian race and were part of the early Indus Valley settlers. Later with the advent of the Aryans, the Dravidians were pushed back into the deep south where they ultimately settled. The present day states of Tamil Nadu, Kerala, Karnataka and Andhra Pradesh constitute the Dravidian culture. Whatever be the historical truth, the identity of the Tamils has largely been forged on this ground. Madurai Meenakshi TempleThe Dravida Nadu of which modern Tamil Nadu formed a part was constituted by various kingdoms such as that of the Pallava, the Chera, the Chola, the Pandya, the Chalukya and the Vijayanagara.The history of Pandyan kingdom dates as early as 6th Century B.C. Madurai was founded by the first Pandyan king Kulasekara.The Pandyas excelled in trade and learning. They controlled the present districts of Madurai and Tirunelveli and part of South Kerala. The Pandyas had trading contacts with Greece and Rome and were powerful in their own right, though they were subjugated during various periods by the Pallavas and Cholas",
+            "fairfestivals":"Tamil Nadu is the origin of Dravidian culture. All the temples and monuments in the state are the best examples of Dravidian architecture. The state has made priceless contributions to the music, art, and literature of India. Bharatnatyam, one of the famous Indian dance forms, has its origin here. Tamil Nadu fairs and festivals are important events that happen round the year and lead to millions of footfalls from different corners of the world.",
+            "Reach":"The Southern Railways has a network that covers a large area of the Southern Peninsula of India. The states of Tamil Nadu, Kerala, Pondicherry, a major portion of Karnataka and a part of Andhra Pradesh are covered by the network. The Chennai Central railway station is just 5 km away from the city centre. Other important rail junctions include Madurai, Tiruchirapalli, Salem, and Coimbatore."
+
+
+        }
+
+        ,
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Golden-Triangle-package.webp",
+            "State":"Golden Triangle Tours",
+            
+
+
+        }
+        ,
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Family-Holidays.jpg",
+            "State":"Family Holiday Tours",
+           
+
+        },
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Winter-Holidays.jpg",
+            "State":"Winter Holidays Tours",
+            
+
+
+        },
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Luxury-Trains-Tours.jpg",
+            "State":"Luxury Trains Tours",
+           
+
+
+        },
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Summer-Holidays.jpg",
+            "State":"Summer Holidays Tours",
+            
+
+
+        },
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Pilgrimage-Tours.jpg",
+            "State":"Pilgrimage Tours",
+            
+
+
+        },
+        {
+            "id":Count++,
+            "category":"static",
+            "Image":"https://www.theindiatourism.com/images/Wildlife-Tours.jpg",
+            "State":"Wild Life Tours",
+           
 
 
         }
